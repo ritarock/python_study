d5aecf8b0351ceebc53a1a1804bd460112fb0291
rita <rita@rita-no-MacBook-Pro.local>
2017-07-26 23:17:19 +0900

    q4

--- q1/q1_04.py
+++ q1/q1_04.py
@@ -1,18 +1,3 @@
-# word = "I am an NLPer"
-
-# word_split = word.split(" ")
-# word_list = list(word)
-
-# def n_gram(word_split,x):
-#     last = len(word_split)
-#     print(last)
-#     ans = []
-#     for i in range(0,last-1):
-#         ans.append(word_split[i:i+1])
-#     return ans
-
-# print(n_gram(word_split,2))
-
 word = "I am an NLPer"
 
 wordSplit = word.split(" ")
@@ -22,9 +7,10 @@ wordList = list(word)
 def n_gram(wordSplit,x):
     last = len(wordSplit)
     ans = []
-    for i in range(0,last-1):
-        ans.append(wordSplit[i:i+2:1])
+    for i in range(0,last-x + 1):
+        ans.append(wordSplit[i:i+x])
     return ans
 
 
 print(n_gram(wordSplit,2))
+print(n_gram(word,2))
