4c812060fc67055ea59a483faed1e75158540538
ryota osaki <r.osaki@ntt.com>
2017-07-28 14:31:10 +0900

    update q1_03

--- q1/q1_03.py
+++ q1/q1_03.py
@@ -1,10 +1,11 @@
-words = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
-words = words.replace(".","").replace(",","")
+words = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+
+words = words.replace(",","").replace(".","")
 words = words.split()
-dict = {}
-for word in range(0,len(words)):
-    if word in [1, 5, 6, 7, 8, 9, 15, 16, 19]:
-        dict[word] = (words[word])[0:1]
-    else:
-        dict[word] = (words[word])[0:2]
-print(dict)
+wlist = []
+
+for word in words :
+    wlist.append(len(word))
+
+print(words)
+print(wlist)
