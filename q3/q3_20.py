70bff0eaf14d2d87effbc645b2e9fcb137ddf4ca
rita <rita@rita-no-MacBook-Pro.local>
2017-08-03 21:19:17 +0900

    q3_20を関数化

--- q3/q3_20.py
+++ q3/q3_20.py
@@ -3,9 +3,11 @@ import json
 
 f = './jawiki-country.json.gz'
 
-with gzip.open(f) as file:
-  for line in file:
-    data_json = json.loads(line)
-    # print(data_json['title'])
-    if data_json['title'] == 'イギリス':
-      print(data_json['text'])
+def uktext(f):
+  with gzip.open(f) as file:
+    for line in file:
+      data_json = json.loads(line)
+      if data_json['title'] == 'イギリス':
+        return (data_json['text'])
+
+print(uktext(f))
