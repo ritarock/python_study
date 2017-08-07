f6d934d3ad39ad397fadb8f1bc14aed7c0e5d80e
rita <rita@rita-no-MacBook-Pro.local>
2017-08-07 21:55:56 +0900

    fix q3_20

--- q3/q3_20.py
+++ q3/q3_20.py
@@ -4,13 +4,6 @@ import re
 
 f = './jawiki-country.json.gz'
 
-# file = gzip.open(f)
-# for line in file:
-#   data_json = json.loads(line)
-#   # print(data_json['title'])
-#   if data_json['title'] == u'イギリス':
-#     text = data_json['text']
-#     break
 def uktext():
   with gzip.open(f) as file:
     for line in file:
