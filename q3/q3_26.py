c85787d8c5c6cc24cbbd2980f3e69aa0dc123f84
rita <wkryz1204@gmail.com>
2017-08-10 23:29:39 +0900

    余分な行を削除

--- q3/q3_26.py
+++ q3/q3_26.py
@@ -7,8 +7,6 @@ f = './jawiki-country.json.gz'
 
 text = (q3_20.uktext()).split('\n')
 
-# print(text)
-
 for i in text:
     category_line = re.search("^(.*?)\s=\s(.*)", i)
     if category_line is not None:
