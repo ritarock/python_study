4ef1717b5e35ab14be1cf502ae40644c4a31c4e4
rita <wkryz1204@gmail.com>
2018-01-26 23:24:34 +0900

    Update prepare.py

--- test_python/prepare.py
+++ test_python/prepare.py
@@ -2,20 +2,24 @@ import os
 import subprocess
 
 def start():
-    print("check git")
+    # print("check git")
     check_git = ['git', '--version']
     try:
         print("installed git")
     except:
         print("install git!")
     home_path = os.path.expanduser('~')
-    path = home_path + "/.vim/pack/mypackage/start/"
-    if os.path.isdir(path):
-        print("FINISH!")
+    start_path = home_path + "/.vim/pack/mypackage/start/"
+    opt_path = home_path + "/.vim/pack/mypackage/opt/myplugin/"
+    if os.path.isdir(start_path):
+        pass
     else:
-        os.makedirs(path)
-        print("FINISH!")
-
+        os.makedirs(start_path)
+    if os.path.isdir(opt_path):
+        pass
+    else:
+        os.makedirs(opt_path)
+    print('FINISH!')
 
 if __name__ == '__main__':
     start()
